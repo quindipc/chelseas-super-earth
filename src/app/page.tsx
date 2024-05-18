7d33eaf60091211257c4ebd986eb0a1b73967528
Chelsea Quindipan <quindipc@gmail.com>
2024-05-18 07:44:37 -0400

    add threejs boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,32 @@ export default function Home() {
             Chelsea's Earth
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ipsum consequatur, amet nihil consectetur, provident tempore accusantium praesentium libero maiores deserunt iste officia cumque possimus nulla ratione veniam, obcaecati corporis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ipsum
+            consequatur, amet nihil consectetur, provident tempore accusantium
+            praesentium libero maiores deserunt iste officia cumque possimus
+            nulla ratione veniam, obcaecati corporis.
           </p>
+          <a
+            href="/earth"
+            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+          >
+            View Here
+            <svg
+              className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+          </a>
         </div>
       </div>
     </>
