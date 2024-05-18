e33ab8fffe9fc695d3f8cc068d4f731bef518e70
Chelsea Quindipan <quindipc@gmail.com>
2024-05-18 07:27:16 -0400

    initial

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <>
+      <div className="mx-auto max-w-7xl p-6 lg:px-8">
+        <div className="mx-auto  lg:mx-0">
+          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            Chelsea's Earth
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ipsum consequatur, amet nihil consectetur, provident tempore accusantium praesentium libero maiores deserunt iste officia cumque possimus nulla ratione veniam, obcaecati corporis.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
