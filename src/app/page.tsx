d4cd22d547f6691c72f0ceae26653ead609f307f
Chelsea Quindipan <quindipc@gmail.com>
2024-05-18 10:58:54 -0400

    add stars & begin to add lights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import getStarfield from "../getStarfield";
 
 import earthTextureImage from "../assets/00_earthmap1k.jpg";
+
 // Extract the URL of the image - note: adding this fixes the GET http://localhost:3000/[object%20Object] 404 (Not Found) problem
 const earthTextureImageUrl = earthTextureImage.src;
-
 
 export default function Home() {
   const mountRef = useRef<HTMLDivElement>(null);
@@ -18,7 +19,7 @@ export default function Home() {
 
     // Create camera with perspective
     const camera = new THREE.PerspectiveCamera(
-      75, // FOV
+      50, // FOV
       window.innerWidth / window.innerHeight,
       0.1, // Near clipping plane
       1000, // Far clipping plane
@@ -26,31 +27,48 @@ export default function Home() {
 
     // Create a WebGL renderer and set its size
     const renderer = new THREE.WebGLRenderer({ antialias: true });
-
     renderer.setSize(window.innerWidth, window.innerHeight);
     mountRef.current?.appendChild(renderer.domElement);
+
+    const earthGroup = new THREE.Group();
+    earthGroup.rotation.z = (-23.4 * Math.PI) / 180;
+    scene.add(earthGroup);
+
+    // Star background
+    const stars = getStarfield();
+    scene.add(stars);
 
     // Initialize OrbitControls to enable camera movement with mouse
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
 
     // Add light
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
-    scene.add(ambientLight);
-
     const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Bright white light
-    directionalLight.position.set(5, 5, 5); // Position the light
+    directionalLight.position.set(-2, 0.5, 1.5); // Position the light
     scene.add(directionalLight);
 
     // Texture loading
     const textureLoader = new THREE.TextureLoader();
     const earthTexture = textureLoader.load(earthTextureImageUrl);
+    // const lightsTexture = textureLoader.load(lightsTextureImageUrl)
 
-    // Creating geometry, material, mesh
+    // Create the lightsMat material
+    const lightsMat = new THREE.MeshBasicMaterial({
+      color: 0x00ff00,
+      transparent: true,
+      opacity: 0.6,
+      // map: new THREE.TextureLoader().load("../"),
+    });
+  
+    
+    // Creating material, mesh
     const geometry = new THREE.IcosahedronGeometry(1, 12);
     const material = new THREE.MeshStandardMaterial({ map: earthTexture });
     const earthMesh = new THREE.Mesh(geometry, material);
-    scene.add(earthMesh);
+    const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+    // Add earthMesh to earthGroup
+    earthGroup.add(earthMesh);
+    scene.add(lightsMesh);
 
     camera.position.z = 5;
 
@@ -58,8 +76,7 @@ export default function Home() {
     const animate = () => {
       requestAnimationFrame(animate);
 
-      earthMesh.rotation.x += 0.001;
-      earthMesh.rotation.y += 0.002;
+      earthGroup.rotation.y += 0.002;
 
       controls.update();
       renderer.render(scene, camera);
@@ -91,7 +108,7 @@ export default function Home() {
             Chelsea's Earth
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Welcome to my model of Earth. Use the mouse to rotate the Earth.
           </p>
         </div>
       </div>
